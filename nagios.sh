188c49d9365278fd8128326e79808e12d48603ef
moejaber89 <85966006+moejaber89@users.noreply.github.com>
2021-06-25 20:28:58 -0400

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
 
 # disable selinux 
-sudo sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
 # you could disable it manually check the manual page if you want to
-sudo yum update
+yum update
 # install required packaging
-sudo yum install -y curl gcc glibc glibc-common wget unzip httpd php gd gd-devel perl postfix
+yum install -y curl gcc glibc glibc-common wget unzip httpd php gd gd-devel perl postfix
 cd /usr/src
 # download nagios source file
-sudo wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
 # unzip the tar file
-sudo tar xzf nagioscore.tar.gz
-sudo cd nagioscore-nagios-4.4.5
+tar xzf nagioscore.tar.gz
+cd nagioscore-nagios-4.4.5
 # compiling nagios
-sudo ./configure
-sudo make all
+./configure
+make all
 # create nagios user and group 
-sudo make install-groups-users
+make install-groups-users
 # add apache user to nagios groups
-sudo usermod -a -G nagios apache
+usermod -a -G nagios apache
 # install nagios binaries html and cgl
-sudo make install
+make install
 # install daemon and enable the service to start on boot
-sudo make install-daemoninit
-sudo systemctl enable httpd.service
+make install-daemoninit
+systemctl enable httpd.service
 # install command mode
-sudo make install-commandmode
+make install-commandmode
 # install sample config file
-sudo make install-config
+make install-config
 # install apache config file
-sudo make install-webconf
+make install-webconf
 # configure firewall and open port 80
-sudo firewall-cmd --zone=public --add-port=80/tcp
-sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
-sudo firewall-cmd --reload
+firewall-cmd --zone=public --add-port=80/tcp
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --reload
 # create nagios web login user
-sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
 # restart apache service
-sudo systemctl restart httpd
+systemctl restart httpd
 # install nagios plugins and packaging
-sudo yum install -y gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils epel-release
-sudo yum install -y perl-Net-SNMP
+yum install -y gcc glibc glibc-common make gettext automake autoconf wget openssl-devel net-snmp net-snmp-utils epel-release
+yum install -y perl-Net-SNMP
 cd /usr/src
-sudo wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
-sudo tar xzf nagios-plugins.tar.gz
+wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
+tar xzf nagios-plugins.tar.gz
 cd nagios-plugins-release-2.2.1
 # compile and install nagios plugin
-sudo ./tools/setup
-sudo ./configure
-sudo make
-sudo make install
+./tools/setup
+./configure
+make
+make install
 # restart nagios service and check the status
-sudo systemctl restart nagios
+systemctl restart nagios
 clear
-sudo systemctl status nagios
+systemctl status nagios
 ip a show enp0s3
 echo " open your web browser in the url section type your centos ip address follow with/nagios"
